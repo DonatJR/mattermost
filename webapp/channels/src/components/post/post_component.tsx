d3912131e78d24aae905289835c0ee07a80723dd
Jonas Reinwald <jonas.reinwald@protonmail.com>
2023-08-22 10:09:41 +0200

    colored thread css class was added to wrong component

--- webapp/channels/src/components/post/post_component.tsx
+++ webapp/channels/src/components/post/post_component.tsx
@@ -284,23 +284,7 @@ const PostComponent = (props: Props): JSX.Element => {
         const hovered =
             hover || fileDropdownOpened || dropdownOpened || a11yActive || props.isPostBeingEdited;
 
-        // === combit-colored-threads begin ===
-        const isComment = (post.root_id && post.root_id.length > 0 && !props.isCollapsedThreadsEnabled) || (props.location === Locations.RHS_COMMENT);
-        let borderLeftColorClass = "";
-        if (isComment) {
-            try {
-                const borderLeftColorIdx = PostUtils.getBorderLeftColor(
-                    post.root_id,
-                    post.channel_id
-                );
-                borderLeftColorClass = `combit-thread-color-${borderLeftColorIdx}`;
-            } catch (e) {
-                console.error("custom thread color error", e);
-            }
-        }
-        // === combit-colored-threads end ===
-
-        return classNames('a11y__section post', borderLeftColorClass /* === combit-colored-threads: added new class ===*/, {
+        return classNames('a11y__section post', {
             'post--highlight': shouldHighlight && !fadeOutHighlight,
             'same--root': hasSameRoot(props),
             'other--root': !hasSameRoot(props) && !isSystemMessage,
@@ -309,7 +293,7 @@ const PostComponent = (props: Props): JSX.Element => {
             'current--user': props.currentUserId === post.user_id && !isSystemMessage,
             'post--system': isSystemMessage || isMeMessage,
             'post--root': props.hasReplies && !(post.root_id && post.root_id.length > 0),
-            'post--comment': isComment /* === combit-colored-threads: condition relocated from here to above ===*/,
+            'post--comment': (post.root_id && post.root_id.length > 0 && !props.isCollapsedThreadsEnabled) || (props.location === Locations.RHS_COMMENT),
             'post--compact': props.compactDisplay,
             'post--hovered': hovered,
             'same--user': props.isConsecutivePost && (!props.compactDisplay || props.location === Locations.RHS_COMMENT),
@@ -414,7 +398,24 @@ const PostComponent = (props: Props): JSX.Element => {
         }
     }, [handleCommentClick, handleJumpClick, props.currentTeam.id, teamId]);
 
-    const postClass = classNames('post__body', {'post--edited': PostUtils.isEdited(post), 'search-item-snippet': isSearchResultItem});
+
+    // === combit-colored-threads begin ===
+    let borderLeftColorClass = "";
+    const isComment = (post.root_id && post.root_id.length > 0 && !props.isCollapsedThreadsEnabled) && !isRHS;
+    if (isComment) {
+        try {
+            const borderLeftColorIdx = PostUtils.getBorderLeftColor(
+                post.root_id,
+                post.channel_id
+            );
+            borderLeftColorClass = `combit-thread-color-${borderLeftColorIdx}`;
+        } catch (e) {
+            console.error("custom thread color error", e);
+        }
+    }
+    // === combit-colored-threads end ===
+
+    const postClass = classNames('post__body', borderLeftColorClass /*combit-colored-threads*/, {'post--edited': PostUtils.isEdited(post), 'search-item-snippet': isSearchResultItem});
 
     let comment;
     if (props.isFirstReply && props.parentPost && props.parentPostUser && post.type !== Constants.PostTypes.EPHEMERAL) {
